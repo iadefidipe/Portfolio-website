cfcaad5c6afef3cbc6a32d7a48c0e688fbb54b6d
Israel Adefidipe <israeladefidipe@gmail.com>
2021-07-03 22:13:58 +0100

    Header == implemented sticky nav on scroll

--- app/js/script.js
+++ app/js/script.js
@@ -3,16 +3,57 @@
 // Dom variables
 
 const header = document.querySelector('.header');
+const nav = document.querySelector('.nav')
 const overlay = document.querySelector('.overlay');
-const btnHamburger = document.getElementById('btnHamburger')
+const btnHamburger = document.getElementById('btnHamburger');
+const allSection = document.querySelectorAll('.section');
 
-btnHamburger.addEventListener('click', function(){
-    if (header.classList.contains('.open')){
-        header.classList.remove('open');
 
-    }
+// btnHamburger.addEventListener('click', function(){
+//     if (header.classList.contains('.open')){
+//         header.classList.remove('open');
 
-    else{
-        header.classList.add('.open');
-    }
-})
+//     }
+
+//     else{
+//         header.classList.add('.open');
+//     }
+// })
+
+
+// // intersection observer 
+// allSections.forEach(function (section) {
+//   sectionObserver.observe(section);
+//   section.classList.add("section--hidden");
+// });
+
+// const revealSection = function (entries, observer) {
+//   const [entry] = entries;
+
+//   if (!entry.isIntersecting) return;
+//   entry.target.classList.remove("section--hidden");
+//   observer.unobserve(entry.target);
+// };
+
+//  const sectionObserver = new IntersectionObserver(revealSection, {
+//    root: null,
+//    threshold: 0.15,
+//  });
+
+ 
+//  TODO: Implementing Sticky navbar
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries; //destructuring entries
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `${navHeight}px`,
+});
+headerObserver.observe(header);
+
